225d79364ee2b7ee18594f4b1e5da040f56b05f9
kylefairbairn <kyle.fairbairn@gmail.com>
2023-04-02 19:49:30 -0400

    updated

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitController from "./controllers/tuits/tuit-controller.js";
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+TuitController(app);
+HelloController(app);
+UserController(app);
+app.listen(process.env.PORT || 4000);
