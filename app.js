6fb23a18e216ae0fab340c6ed1bd353d4075206d
kylefairbairn <kyle.fairbairn@gmail.com>
2023-04-09 19:39:09 -0400

    a9

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitController from "./controllers/tuits/tuit-controller.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://127.0.0.1:27017/tuiter'
+
+
+const CONNECTION_STRING = "mongodb+srv://kyle:isawesome@cluster0.0lvcoyg.mongodb.net/?retryWrites=true&w=majority"
     || 'mongodb://127.0.0.1:27017/tuiter'
+
+
+
 
 mongoose.connect(CONNECTION_STRING);
 
